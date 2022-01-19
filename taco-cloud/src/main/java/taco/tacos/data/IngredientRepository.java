a8c6009c1aa8599caf83c8aebc9ddf9e55807a40
sabiutayo <sabiutayo@yahooo.com>
2022-01-19 15:05:11 +0200

    jdbc implemnting

---
+++ taco-cloud/src/main/java/taco/tacos/data/IngredientRepository.java
@@ -0,0 +1,14 @@
+package taco.tacos.data;
+
+import taco.tacos.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+
+    Iterable<Ingredient> findAll();
+
+    Optional<Ingredient> findById(String id);
+
+    Ingredient save(Ingredient ingredient);
+}
